53785c677bb9807c83d50b6a9692b945cfc3d3d7
ParkYongNam <dkskcka3@naver.com>
2023-11-07 11:28:14 +0900

    슬라이드 fade

--- js/web.js
+++ js/web.js
@@ -6,14 +6,28 @@ $(document).ready(function () {
     // $(태그).css(스타일, 스타일값)
     // $(옮길테그).appendTo($(부모가될태그))
 
-    const aniw = $(".swiper-slide").width(); // $(".swiper-slide").css("width");
+    var num = 0;
+
+    if ($(".swiper-slide").length > 0) {
+        setInterval(fadefun, 3000)
+    }
+    // function aniRightLeft() {
+    //     $(".swiper-wrapper").animate({ "marginLeft": -1200 }, 400, function () {
+    //         $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
+    //        // $(".swiper-slide").eq(0).appendTo($(this))
+    //        // $(".swiper-slide:nth-child(1)").appendTo($(".swiper-wrapper"))
+    //        // $(this).find(">div").eq(0).appendTo($(this))
+    //        // $(">div",this).eq(0).appendTo($(this))
+    //        // $(">div:first-child",this).appendTo($(this))
+    //         $(".swiper-wrapper").css("marginLeft", 0)
+    //     })
+    // }
 
 
-    setInterval(function () {
-        $(".swiper-wrapper").animate({ "marginLeft": -aniw }, 400, function () {
-            $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"))
-            $(".swiper-wrapper").css("marginLeft", 0)
-        })
-    }, 3000)
 
+    function fadefun() {
+        num++;
+        num = num % $(".swiper-slide").length
+        $(".swiper-slide").eq(num).addClass("act").siblings().removeClass("act")
+    }
 })
